1ef60e9d5e670a7cc98c8b3f3787029ee99e4040
Даниил <dgandapas1@gmail.com>
2024-05-31 11:16:08 +0300

    commit

---
+++ DiscordLiker/Program.cs
@@ -0,0 +1,15 @@
+﻿var token = Console.Read().ToString();
+
+
+
+
+var client = new HttpClient();
+var request = new HttpRequestMessage(HttpMethod.Put, "https://discord.com/api/v9/channels/1245967394761871381/messages/1245969386016346214/reactions/%F0%9F%91%8D/%40me?location=Message&type=0");
+request.Headers.Add("Host", "discord.com");
+request.Headers.Add("Connection", "keep-alive");
+request.Headers.Add("Referer", "https://discord.com/channels/@me/1245967394761871381");
+request.Headers.Add("Authorization", token);
+request.Headers.Add("Cookie", "__cfruid=6e665a5ac458e0ecc81134985c607b05cf39d545-1717133465; __dcfduid=f9c210061f0e11ef85962222dbdcba35; __sdcfduid=f9c210061f0e11ef85962222dbdcba3559d3f61a1e061afcf7ea120a5dfa93ff9afa8d86eafc62db5aed9fdbff5ea159; _cfuvid=2xH1Mz7lg1gw04lHpICGdG3ta4TobHDnSiLBrWxGpcA-1717133465264-0.0.1.1-604800000");
+var response = await client.SendAsync(request);
+response.EnsureSuccessStatusCode();
+Console.WriteLine(await response.Content.ReadAsStringAsync());
